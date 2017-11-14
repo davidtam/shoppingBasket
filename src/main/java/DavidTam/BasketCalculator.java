465155fe174e2ca7d816b9751fd5f0833dde0c08
davidtambjss <david.tam@bjss.com>
2017-11-14 22:06:54 +0000

    Added main

--- src/main/java/DavidTam/BasketCalculator.java
+++ src/main/java/DavidTam/BasketCalculator.java
@@ -2,6 +2,10 @@ package DavidTam;
 
 import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
 
 public class BasketCalculator {
 
@@ -12,4 +16,19 @@ public class BasketCalculator {
                 EnumSet.allOf(Offer.class).stream()
                         .mapToLong(offer -> offer.countDiscount(basket) * offer.getDiscount()).sum();
     }
+
+    public static void main(String args[]){
+
+        if (args.length != 1){
+            System.out.println("Usage:");
+            System.out.println("Usage:java DavidTam.BasketCalculator \"APPLE, BANANA\"");
+            System.out.println("Usage:");
+        } else {
+            long totalCost = calculate(asList(args[0].split(",")).stream()
+                    .map(str -> str.trim().toUpperCase()).map(name -> Item.valueOf(name)).collect(Collectors.toList()));
+            System.out.println(format("Basket of \"%s\" cost %d pence", args[0], totalCost));
+
+        }
+    }
+
 }
