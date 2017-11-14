58985fcbce0a272e404f45efe202a9c88eda0e13
davidtambjss <david.tam@bjss.com>
2017-11-14 20:46:21 +0000

    added pom and other simple structure

--- src/main/java/DavidTam/Item.java
+++ src/main/java/DavidTam/Item.java
@@ -1,4 +1,25 @@
-package DavidTam.old;
+package DavidTam;
 
-public class Items {
+public enum Item {
+
+    APPLE(35),
+    BANANA(20),
+    MELON(50),
+    LIME(15);
+
+    private int priceInPence;
+
+
+    Item(int priceInPence) {
+        this.priceInPence = priceInPence;
+    }
+
+    /**
+     *
+     * @return price in Pence
+     */
+    public int getPrice() {
+        return priceInPence;
+    }
+
 }
--- src/test/java/DavidTam/ItemTest.java
+++ src/test/java/DavidTam/ItemTest.java
@@ -1,4 +1,20 @@
 package DavidTam;
 
-public class ItemTests {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ItemTest {
+
+
+    @Test
+    public void testItemsAndPricesAreSetupCorrectly(){
+
+        assertEquals(35, Item.valueOf("APPLE").getPrice());
+        assertEquals(20, Item.valueOf("BANANA").getPrice());
+        assertEquals(50, Item.valueOf("MELON").getPrice());
+        assertEquals(15, Item.valueOf("LIME").getPrice());
+
+    }
+
 }
